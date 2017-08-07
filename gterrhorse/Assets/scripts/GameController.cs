9a0cd7cec779c4ed0bcafe0dd47453e140714bb9
NICK-PC\Nick <nickfro@gmail.com>
2017-08-07 09:36:29 -0400

    Added MORE files oh boy

---
+++ gterrhorse/Assets/scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public TextMesh scoreText;
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        scoreText = GetComponent<TextMesh>();
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        Debug.Log("SCORE" + score);
+        scoreText.text = "Score: " + score;
+    }
+}
