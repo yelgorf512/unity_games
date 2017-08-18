d17b90538df4002f2e453fa2da5aa842fc48fda2
NICK-PC\Nick <nickfro@gmail.com>
2017-08-17 23:55:08 -0400

    Added working panning effect with gamepad

--- gterrhorse/Assets/scripts/CameraControl.cs
+++ gterrhorse/Assets/scripts/CameraControl.cs
@@ -6,26 +6,30 @@ public class CameraControl : MonoBehaviour
 {
 
     public GameObject player;
+
     private Vector3 offset;
-    
     private Vector3 lookat_holder;
-    private float x_offset;
+    private Vector3 origin_position;
+
+    private float pan_x_offset;
+    private float pan_x_offset_change;
     private float pan_rate;
+    private float pan_limit;
+
     private float cameraMoveHorizontal;
-    private bool spec_cameraMoveHorizontal;
+    
 
     private bool pan_left, pan_right, pan_up, pan_down;
     private bool correcting;
 
-
-    Transform old_transform;
-
+    
     // Use this for initialization
     void Start()
     {
-        x_offset = 0;
-        pan_rate = 0.05f;
-        correcting = false;
+        pan_x_offset = 0;
+        pan_rate = 0.3f;
+        origin_position = transform.localPosition;
+        //correcting = false;
     }
 
     private void Update()
@@ -33,21 +37,25 @@ public class CameraControl : MonoBehaviour
         cameraMoveHorizontal = Input.GetAxis("Horizontal");
         //Debug.Log(cameraMoveHorizontal);
 
+        
+
+        /*
         if (Mathf.Abs(cameraMoveHorizontal) > .8f)
         {
-            if (cameraMoveHorizontal > .8f)
+            if (cameraMoveHorizontal < .8f)
             {
                 pan_right = true;
                 pan_left = false;
                 Debug.Log("contoller pan_right");
             }
-            else if (cameraMoveHorizontal < -.8f)
+            else if (cameraMoveHorizontal > -.8f)
             {
                 pan_left = true;
                 pan_right = false;
                 Debug.Log("controller pan_left");
             }
         }
+        
         else
         {
             Debug.Log("HERE");
@@ -74,27 +82,53 @@ public class CameraControl : MonoBehaviour
             {
                 correcting = false;
             }
-        }
+        }*/
     }
 
 
     // Update is called once per frame
     void LateUpdate()
     {
+        if (Mathf.Abs(cameraMoveHorizontal) > .8f)
+        {
+            pan_x_offset_change = -cameraMoveHorizontal;
+        }
+        else
+        {
+            if (pan_x_offset < -0.3f)
+            {
+                pan_x_offset += 1;
+            }
+            else if (pan_x_offset > 0.3f)
+            {
+                pan_x_offset -= 1;
+            }
+            else
+            {
+                pan_x_offset = 0;
+            }
+        }
 
-        old_transform = transform;
-
-        //cam_position_x restricted between -6 and 6
         transform.LookAt(transform);
-
         
+        pan_x_offset += pan_x_offset_change * pan_rate;
 
-        //if (pan_left && (transform.position.x - pan_rate > -3))
+        pan_x_offset = Mathf.Clamp(pan_x_offset, -3, 3);
+
+        transform.localPosition = new Vector3(Mathf.Clamp(origin_position.x + pan_x_offset, -3, 3), transform.localPosition.y, transform.localPosition.z);
+
+        Debug.Log("pan_x_offset " + pan_x_offset);
+        Debug.Log("pan_x_offset_change " + pan_x_offset_change);
+        Debug.Log("origin_position.x " + origin_position.x);
+        Debug.Log("pan_x_offset_change " + pan_x_offset_change);
+        Debug.Log("cameraMoveHorizontal" + cameraMoveHorizontal);
+
+
+        /*
         if (pan_left)
         {
             transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x - pan_rate, -3, 3) , transform.localPosition.y, transform.localPosition.z);
         }
-        //else if (pan_right && (transform.position.x + pan_rate < 3))
         else if (pan_right)
         {
             transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x + pan_rate, -3, 3), transform.localPosition.y, transform.localPosition.z);
@@ -106,32 +140,6 @@ public class CameraControl : MonoBehaviour
         else if (pan_down)
         {
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - pan_rate, transform.localPosition.z);
-        }
-        //cameraMoveHorizontal = Input.GetAxis("Horizontal");
-
-        /*cameraMoveHorizontal = 1;
-        
-        if (Mathf.Abs(cameraMoveHorizontal) < .5f)
-        {
-            Debug.Log("should be moving camera" + x_offset);
-            x_offset += cameraMoveHorizontal;
-            transform.position = new Vector3(transform.position.x + Mathf.Clamp(x_offset, -.1f, .1f), old_transform.position.y, old_transform.position.z);
         }*/
-        /*
-        else if (x_offset > 0)
-        {
-            Debug.Log("reset x over");
-            transform.position = new Vector3(0, old_transform.position.y, old_transform.position.z);
-            x_offset = 0;
-        }
-        else if (x_offset < 0)
-        {
-            Debug.Log("reset x under");
-            transform.position = new Vector3(transform.position.x - x_offset, old_transform.position.y, old_transform.position.z);
-            x_offset = 0;
-        }*/
-
-
-
     }
 }
